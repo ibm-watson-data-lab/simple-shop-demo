0a033459c0b1dffc0fb30f352a63f7de34b57ab8
Matt Collins <matt@strikesandgutters.com>
2017-01-18 14:38:56 +0000

    Layout, cards

---
+++ js/sample-data.js
@@ -0,0 +1,115 @@
+var sampleData = [
+	
+	{
+	  "_id": "110357",
+	  "_rev": "1-fdb5d4b867e7562b0952ee3ab86de1cd",
+	  "title": "The Lion King",
+	  "year": "1994",
+	  "rating": 8.5,
+	  "runtime": "89 min",
+	  "genre": [
+	    "Animation",
+	    "Adventure",
+	    "Drama"
+	  ],
+	  "director": "Roger Allers, Rob Minkoff",
+	  "cast": [
+	    "Jonathan Taylor Thomas",
+	    "Matthew Broderick",
+	    "James Earl Jones",
+	    "Jeremy Irons"
+	  ],
+	  "poster": "http://ia.media-imdb.com/images/M/MV5BMjEyMzgwNTUzMl5BMl5BanBnXkFtZTcwNTMxMzM3Ng@@._V1_SX300.jpg",
+	  "price": 13.99
+	},
+
+	{
+	  "_id": "87469",
+	  "_rev": "1-21ec837f1ff85d1d688e44c0419e3141",
+	  "title": "Indiana Jones and the Temple of Doom",
+	  "year": "1984",
+	  "rating": 7.6,
+	  "runtime": "118 min",
+	  "genre": [
+	    "Action",
+	    "Adventure"
+	  ],
+	  "director": "Steven Spielberg",
+	  "cast": [
+	    "Harrison Ford",
+	    "Kate Capshaw",
+	    "Jonathan Ke Quan",
+	    "Amrish Puri"
+	  ],
+	  "poster": "http://ia.media-imdb.com/images/M/MV5BMTMyNzI4OTA5OV5BMl5BanBnXkFtZTcwMDQ2MjAxNA@@._V1_SX300.jpg",
+	  "price": 5.99
+	},
+
+	{
+	  "_id": "407887",
+	  "_rev": "1-fd6fec416b29d0f0621b00fefdc430f6",
+	  "title": "The Departed",
+	  "year": "2006",
+	  "rating": 8.5,
+	  "runtime": "151 min",
+	  "genre": [
+	    "Crime",
+	    "Drama",
+	    "Thriller"
+	  ],
+	  "director": "Martin Scorsese",
+	  "cast": [
+	    "Leonardo DiCaprio",
+	    "Matt Damon",
+	    "Jack Nicholson",
+	    "Mark Wahlberg"
+	  ],
+	  "poster": "http://ia.media-imdb.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_SX300.jpg",
+	  "price": 5.99
+	},
+
+	{
+	  "_id": "118715",
+	  "_rev": "1-4ac9cc87886fe3137b49d41fb2cd89c2",
+	  "title": "The Big Lebowski",
+	  "year": "1998",
+	  "rating": 8.2,
+	  "runtime": "117 min",
+	  "genre": [
+	    "Comedy",
+	    "Crime"
+	  ],
+	  "director": "Joel Coen, Ethan Coen",
+	  "cast": [
+	    "Jeff Bridges",
+	    "John Goodman",
+	    "Julianne Moore",
+	    "Steve Buscemi"
+	  ],
+	  "poster": "http://ia.media-imdb.com/images/M/MV5BMTQ0NjUzMDMyOF5BMl5BanBnXkFtZTgwODA1OTU0MDE@._V1_SX300.jpg",
+	  "price": 6.99
+	}
+
+]
+
+var cardData = [
+
+	{
+		title: "Card 1",
+		description: "This is where we write all about card 1",
+		cta: { 
+			url: "http://www.google.com",
+			label: "Google"
+		}
+	},
+
+	{
+		title: "Card 2",
+		description: "This is where we write all about card 2",
+		cta: { 
+			url: "http://www.microsoft.com",
+			label: "Microsoft"
+		}
+	}
+
+]
